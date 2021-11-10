798312b23e61d694a3a261d72809630e68cf6d12
Денис Гришкевич <cris12@mail.ru>
2021-11-10 21:39:02 +0300

    first commit ver 1

---
+++ src/main/java/domain/Wallpost.java
@@ -0,0 +1,17 @@
+package domain;
+
+public class Wallpost {
+    private String id;
+    private String title;
+    private String date;
+    private String user;
+    private String menu;
+    private String views;
+    private String comments;
+    private String share;
+    private String emoji;
+    private String photoId;
+    private String state;
+    private String text;
+    private String phone;
+}
---
+++ src/main/java/domain/WallpostManager.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class WallpostManager {
+    private Wallpost[] wall;
+    public Wallpost[] getWallpostForFeed(){
+        return null;
+
+    }
+}
